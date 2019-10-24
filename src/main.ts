876697568dfb9a2864f631f66e973b3ffc3f2994
Faithful-Mind <lpyy15@gmail.com>
2019-10-24 18:31:04 +0800

    引入Element UI

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui'; // 引入element-ui
+import 'element-ui/lib/theme-chalk/index.css';
+
 import App from './App.vue';
 import router from './router';
 
+Vue.use(ElementUI); // Vue全局使用
 Vue.config.productionTip = false;
 
 new Vue({
