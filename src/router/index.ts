14daa9b0fa0c06645030a9bcab5504bc5ccbd257
Faithful-Mind <lpyy15@gmail.com>
2019-10-25 03:13:06 +0800

    跳转拦截

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,4 +22,18 @@ const router = new VueRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) => {
+  const token = sessionStorage.getItem('demo-token');
+  if (to.path === '/') { // 如果是跳转到登录页的
+    if (token !== 'null' && token != null) {
+      next('/todolist'); // 如果有token就转向todolist不返回登录页
+    }
+    next(); // 否则跳转回登录页
+  } else if (token !== 'null' && token != null) {
+    next(); // 如果有token就正常转向
+  } else {
+    next('/'); // 否则跳转回登录页
+  }
+});
+
 export default router;
