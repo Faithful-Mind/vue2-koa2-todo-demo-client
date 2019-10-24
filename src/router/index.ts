01835f3132a2d29952469b5b84e283c197ad5313
Faithful-Mind <lpyy15@gmail.com>
2019-10-25 05:36:37 +0800

    自动将Token加入全局Headers

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import axios from 'axios';
+
 import Login from '../views/Login.vue';
 
 Vue.use(VueRouter);
@@ -30,6 +32,8 @@ router.beforeEach((to, from, next) => {
     }
     next(); // 否则跳转回登录页
   } else if (token !== 'null' && token != null) {
+    // 全局设定header的token验证，注意Bearer后有个空格
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
     next(); // 如果有token就正常转向
   } else {
     next('/'); // 否则跳转回登录页
